d66b31b1d4c11a93b1df8a18f12a814693a90367
Kacper <kacperceteraonett@gmail.com>
2026-04-06 08:07:57 +0200

    Change scripts/client.py

--- scripts/core/commands.py
+++ scripts/core/commands.py
@@ -1,3 +1,6 @@
+import os
+import subprocess
+import pyperclip
 from .config import update_config
 
 # Login configuration command
@@ -15,3 +18,39 @@ def cmd_init_setup():
     update_config(new_data)
     print("Configuration saved succesfully!")
 
+# NOTE these are for later recofiguration 
+def notify(title, message):
+    # Native macOS notifications (no extra packages needed)
+    os.system(f"osascript -e 'display notification \"{message}\" with title \"{title}\"'")
+
+def cmd_login():
+    ensure_config()
+    editor = os.environ.get('EDITOR', 'nano')
+    print(f"Opening config file in {editor}...")
+    subprocess.call([editor, str(CONFIG_FILE)])
+
+def cmd_send():
+    text = pyperclip.paste()
+    if not text.strip():
+        print("Clipboard is empty.")
+        return
+
+    print("Sending clipboard to cloud...")
+    cloud = CloudClient()
+    if cloud.upload(text):
+        print("Success: Clipboard sent.")
+        notify("CloudBridge", "Clipboard sent to the cloud.")
+    else:
+        print("Failed to send clipboard.")
+
+def cmd_get():
+    print("Fetching clipboard from cloud...")
+    cloud = CloudClient()
+    text = cloud.download()
+    
+    if text:
+        pyperclip.copy(text)
+        print("Success: Clipboard updated.")
+        notify("CloudBridge", "New clipboard received.")
+    else:
+        print("Failed to fetch clipboard or cloud is empty.")
---
+++ scripts/core/main.py
@@ -0,0 +1,28 @@
+import json 
+from webdav3.client import Client
+import os
+from .config import get_config
+
+class ClientCloud:
+    def __init__(self):
+        conf = get_config()
+        options = {
+            "webdav_hostname":  conf.get("webdav_hostname", ""),
+            "webdav_login":     conf.get("webdav_login", ""),
+            "webdav_password":  conf.get("webdav_password", "")
+        }
+        self.client = Client(options)
+        self.remote_path = "clipboard_sync.txt"
+    
+    def upload(self, text):
+        tmp_file = "tmp_upload.txt"
+        with open(tmp_file, "w", encoding = "utf-8") as file:
+            file.write(text)
+        try: 
+            self.client.upload_sync(remote_path = self.remote_path, local_path = tmp_file)
+            return True
+        except Exception as e:
+            print(f"Error {e}")
+        finally: 
+            if os.path.exists(tmp_file):
+                os.remove(tmp_file)
